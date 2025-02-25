144669adbdc268642c0fded661365cb5c348e1de
Bjarne Helgesen <bjarne@helgesen.biz>
2025-02-25 17:38:36 +0100

    Added example of eval and f-strings

---
+++ input_output2.py
@@ -0,0 +1,11 @@
+# Showing eval, consitional statements, and f-strings
+
+# == > < >= <= != 
+# and or not
+
+value = eval(input("Enter an odd number: "))
+
+if value <= 5 or value >= 17:
+    print("hunting high and low.")
+my_string = f"Your number is {value:.2f}."
+

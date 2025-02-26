f025b0665d754066c5a14a555e0317b595896a6c
Bjarne Helgesen <bjarne@helgesen.biz>
2025-02-26 19:20:38 +0100

    Add string tests

--- string_methods.py
+++ string_methods.py
@@ -16,3 +16,19 @@ for c in greeting:
     print(c)
 
 print(greeting[1]) # 'e'
+
+# string testing functions
+"hello".isalpha() # True
+"hello123".isalpha() # False
+"123".isdigit() # True
+"hello123".isdigit() # False
+"hello123".isalnum() # True
+"hello".isalnum() # True
+"123".isalnum() # True
+"123! #".isalnum() # False
+"hello".islower() # True
+"HELLO".isupper() # True
+"hello".startswith("he") # True
+"hello".endswith("lo") # True
+"hello".isascii() # True
+    
